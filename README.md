3b8b73e90c1c07802c484813b755848962574cfb
noahhelton98 <noahhelton98@gmail.com>
2021-04-01 21:14:11 -0400

    made functions for season averages and todays scores

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NBA_stats
+
+
+
+link: https://noahhelton98.github.io/Stats-from-the-Key/
--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,16 @@
-
+//get variables
 var url = 'https://www.balldontlie.io/api/v1/'
 var buttonTab = $('#allTeams')
 var currentdate = moment().format('YYYY-MM-DD');
 
 var dateEl = $('#currentDayDisplay')
-
-dateEl.text(currentdate).css('color:black');
-
+//set the date on top of the 
+dateEl.text( moment().format("MMM Do, YYYY"));
 let teams = [];
 
-/* fetch('https://www.balldontlie.io/api/v1/games?start_date=[]2021-03-29&end_date=[]2021-03-29', {
-  method: 'GET'
-}).then(function (response) {
-    return response.json();
-}) .then(function (data) {
-    console.log(data);
-  });
- */
 
 
+//Fetch the teams and display them as buttons like a nav bar vertically
 fetch('https://www.balldontlie.io/api/v1/teams', {
   method: 'GET'
 }).then(function (response) {
@@ -33,9 +25,7 @@ fetch('https://www.balldontlie.io/api/v1/teams', {
     buttonTab.append(`<button class="btn btn-primary" type="button">${teams[i]['full_name']} </button>`)
   }
   var teamBtns = $('#allTeams button')
-
-  console.log(teamBtns.text())
-
+//Tell the btn what to do on click event
   teamBtns.on('click', function(event){
     event.preventDefault();
     var t = $(this).index() + 1
@@ -43,12 +33,11 @@ fetch('https://www.balldontlie.io/api/v1/teams', {
 
   })
 
-
   }) 
 
 
 
-
+//Display the team names function (for the buttons in the initial fetch statement)
 function displayTeams(teamName){
   fetch(`https://www.balldontlie.io/api/v1/teams/${teamName}`, {
     method: 'GET'
@@ -60,4 +49,80 @@ function displayTeams(teamName){
 }
 
 
-console.log(currentdate)
+function getPlayers (teamName){
+  //fetch('https://www.balldontlie.io/api/v1/players')
+  return true
+}
+
+function getStats(team, player){
+  //fetch
+  return true
+}
+
+
+//Get the average of a single player if full name is typed in
+function getSeasonAverageSinglePlayer(player){
+  //fetch ('https://www.balldontlie.io/api/v1/season_averages?season=2020', {
+   
+  fetch (`https://www.balldontlie.io/api/v1/players?search=${player}`, {  
+    method: 'GET'
+  }).then (function (response) { 
+    return response.json();
+  }).then(function (data){
+    console.log(data.data)
+    var id = data.data[0].id
+
+    //Maybe make a drop down menu of all the seasons and allow you to choose which one you want to look at, have default be the last 5
+    var currentYear = moment().format('YYYY')
+    var seasons = []
+    console.log(seasons)
+    for (var i =1; i < 6; i++){
+      seasons.push(currentYear - i)
+    }
+
+    for (var i =0; i < seasons.length; i++){
+      console.log(seasons[i])
+  fetch (`https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${id}&season=${seasons[i]}`, {  
+      method: 'GET'
+  }).then(function (response){
+    return response.json();
+  }).then (function (data){
+    console.log(data.data)
+
+  })
+}
+})
+};
+
+getSeasonAverageSinglePlayer('Anthony Davis')
+
+
+//Get the score for the current date and then add 
+function getTodaysScores(date){
+  fetch(`https://www.balldontlie.io/api/v1/games?start_date=[]${date}&end_date=[]${date}`, {
+  method: 'GET'
+}).then(function (response) {
+    return response.json();
+}) .then(function (data) {
+    console.log(data.data[0].home_team.full_name);
+    for (var i = 0; i < data.data.length; i++){
+      console.log(data.data[i].home_team.full_name, data.data[i].visitor_team.full_name)
+      console.log(data.data[i].home_team_score, data.data[i].visitor_team_score)
+      console.log(data.data[i].period)
+    }
+  });
+
+}
+
+function getLastFiveScores(date, team){
+  return true;
+}
+
+
+function setScores(){
+  return true;
+}
+
+getTodaysScores(currentdate);
+
+
